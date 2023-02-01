f0e7159bcedce069639e851c12967cb31ef050c4
VanshulB <vanshulbhatia@gmail.com>
2023-02-02 00:31:00 +0530

    graphql server setup (type-graphql)

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,28 @@
 import { MikroORM } from "@mikro-orm/core";
-import { Post } from "./entities/Post";
 import MikroOrmConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
-	const orm = await MikroORM.init(MikroOrmConfig);
-	await orm.getMigrator().up();
-	const emFork = orm.em.fork();
+  const orm = await MikroORM.init(MikroOrmConfig);
+  await orm.getMigrator().up();
+  const app = express();
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
 
-	const post = emFork.create(Post, {
-		title: 'my first post',
-		createdAt: "",
-		updatedAt: ""
-	});
-	await emFork.persistAndFlush(post)
-
-	const posts = await emFork.find(Post, {});
-	console.log(posts);
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
 };
 
 main().catch((err) => {
-	console.error(err);
-})
+  console.error(err);
+});
