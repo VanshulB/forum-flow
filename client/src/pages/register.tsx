23dc5e3aa65c806dfde2a7a3c2a6bc463dabb246
VanshulB <vanshulbhatia@gmail.com>
2023-03-23 17:14:37 +0530

    error handling in forms

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -5,6 +5,7 @@ import Wrapper from "@/components/Wrapper";
 import InputField from "@/components/InputField";
 import { useMutation } from "urql";
 import { useRegisterMutation } from "@/generated/graphql";
+import { toErrorMap } from "@/utils/toErrorMap";
 
 interface registerProps {}
 
@@ -31,8 +32,12 @@ const Register: React.FC<registerProps> = ({}) => {
       <Wrapper variant="small">
         <Formik
           initialValues={{ username: "", password: "" }}
-          onSubmit={async (values) => {
+          onSubmit={async (values, { setErrors }) => {
             const response = await register(values);
+            console.log(response);
+            if (response.data?.register.errors) {
+              setErrors(toErrorMap(response.data.register.errors));
+            }
           }}
         >
           {({ isSubmitting }) => (
